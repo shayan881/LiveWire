6ddc7eb22cccc9cf60163c919bb336f7eee6aa77
shayan <shayanheydari1121@gmail.com>
2025-06-06 21:56:31 +0330

    session 57

--- app/Livewire/CreateCourse.php
+++ app/Livewire/CreateCourse.php
@@ -3,21 +3,30 @@
 namespace App\Livewire;
 
 use App\Models\Course;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreateCourse extends Component
 {
+
+    // #[Validate('required')]
+    // #[Validate('required', onUpdate: false)]
+    // #[Validate('required', as: 'نام')]
+    // #[Validate('required', message: 'پر کن دیگه بابا')]
+    #[Validate('min:3', translate: false)]
     public $name = '';
+    #[Validate('required')]
     public $price = '';
 
     public function save()
     {
+        $this->validate();
         $inputs = ['name' => $this->name, 'price' => $this->price];
-        $validated = $this->validate([
-            'name' => 'required',
-            'price' => 'required'
-        ]);
-        Course::create($validated);
+        // $validated = $this->validate([
+        //     'name' => 'required',
+        //     'price' => 'required'
+        // ]);
+        Course::create($inputs);
         return to_route('courses')->with('success', 'دوره با موفقیت ساخته شد');
     }
     public function render()
